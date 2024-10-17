a33529c2378ad9088dba3bc8fd8ce99b925fbece
w-e-w <40751091+w-e-w@users.noreply.github.com>
2024-10-17 15:51:42 +0900

    Update demo_version_selector.py

--- scripts/demo_version_selector.py
+++ scripts/demo_version_selector.py
@@ -1,3 +1,15 @@
+"""
+This is a demo extension demonstrate a method of enabling or disabling scripts.Script
+
+The concept is simple you create a setting entry that allows the user to select the scripts / module that they wish to use
+then fetch the module class and store it under a global variable of this file
+
+To be a scripts.Script loaded by webui, it simply needs to store in a global variable in any python script under <extensions>/<extensions_name>/scripts/<your_python_script>.py
+Other class such as Script scripts_postprocessing.ScriptPostprocessing can also be loaded in this fashion
+
+This is simply a concept demo script, you don't have to structure extensions in the same way, other methods can achieve similar effect
+"""
+
 from multi_version_extension_demo_modules import single_modules, multi_modules
 from modules import shared, ui_components
 import gradio as gr
